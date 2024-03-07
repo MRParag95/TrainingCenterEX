760318ef5843127e0a2e9db3f57638de2d5f2b44
MRParag95 <rahman.parag.95@gmail.com>
2024-03-07 17:41:30 +0600

    Added Coordinator Entity.

---
+++ src/main/java/com/mendax47/trainingcenter/entity/Coordinator.java
@@ -0,0 +1,17 @@
+package com.mendax47.trainingcenter.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "coordinator")
+public class Coordinator extends Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "coordinator_seq")
+    @SequenceGenerator(name = "coordinator_seq")
+    @Column(name = "id", nullable = false)
+    private Long id;
+}
