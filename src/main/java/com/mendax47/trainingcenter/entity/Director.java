c65611ad2ab6c90d40e618985298350ea5721c1f
MRParag95 <rahman.parag.95@gmail.com>
2024-03-07 17:39:45 +0600

    Added Director Entity.

---
+++ src/main/java/com/mendax47/trainingcenter/entity/Director.java
@@ -0,0 +1,23 @@
+package com.mendax47.trainingcenter.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "director")
+public class Director extends Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "director_seq")
+    @SequenceGenerator(name = "director_seq")
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+}
