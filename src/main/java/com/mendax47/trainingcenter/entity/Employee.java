76db1f21f9ccf003eeff9e33f6e84d3a07cf4a4d
MRParag95 <rahman.parag.95@gmail.com>
2024-03-07 17:34:43 +0600

    Updated Employee Entity.

--- src/main/java/com/mendax47/trainingcenter/entity/Employee.java
+++ src/main/java/com/mendax47/trainingcenter/entity/Employee.java
@@ -1,11 +1,17 @@
 package com.mendax47.trainingcenter.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 @MappedSuperclass
 public class Employee {
     @Column(name = "first_name", nullable = false, length = 100)
